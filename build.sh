570854eebd11c0090722bb99d5eb2e78fa39e937
astarryknight <whiteh4tter@gmail.com>
2025-06-23 13:17:48 -0400

    finish ci tests and update build script

--- build.sh
+++ build.sh
@@ -25,34 +25,34 @@ while getopts ${OPTSTRING} opt; do
   esac
 done
 
-# # #version bumping
-# VERSION_OLD=$(sed -n '7p' pyproject.toml | awk -F'"' '{print $2}')
+# #version bumping
+VERSION_OLD=$(sed -n '7p' pyproject.toml | awk -F'"' '{print $2}')
 
-# if [ -z "${VERSION+xxx}" ]; then
-#     maj=$(echo $VERSION_OLD | cut -d '.' -f 1)
-#     min=$(echo $VERSION_OLD | cut -d '.' -f 2)
-#     bug=$(echo $VERSION_OLD | cut -d '.' -f 3)
-#     VERSION="$maj.$min.$(($bug+1))"
-# fi
+if [ -z "${VERSION+xxx}" ]; then
+    maj=$(echo $VERSION_OLD | cut -d '.' -f 1)
+    min=$(echo $VERSION_OLD | cut -d '.' -f 2)
+    bug=$(echo $VERSION_OLD | cut -d '.' -f 3)
+    VERSION="$maj.$min.$(($bug+1))"
+fi
 
-# echo $VERSION
+echo $VERSION
 
-# #replace version number in toml file
-# sed -i.bu "7s/.*/version = \"$VERSION\"/" pyproject.toml
+#replace version number in toml file
+sed -i.bu "7s/.*/version = \"$VERSION\"/" pyproject.toml
 
-# #move old builds from dist to archive
-# mv dist/* archive
+#move old builds from dist to archive
+mv dist/* archive
 
-# #building
-# python3 -m pip install --upgrade build
-# python3 -m build
+#building
+python3 -m pip install --upgrade build
+python3 -m build
 
-# #uploading to pypi
-# python3 -m pip install --upgrade twine
-# python3 -m twine upload --repository testpypi dist/* --verbose
+#uploading to pypi
+python3 -m pip install --upgrade twine
+python3 -m twine upload --repository testpypi dist/* --verbose
 
 
-# #create github release
-# if [ "$RELEASE" = true ]; then
-#     gh release create v$VERSION ./dist/*.gz --latest -n "# stackcore $VERSION Release Notes:" -t "v$VERSION ($(date '+%m/%d/%Y'))"
-# fi
+#create github release
+if [ "$RELEASE" = true ]; then
+    gh release create v$VERSION ./dist/*.gz --latest -n "# stackcore $VERSION Release Notes:" -t "v$VERSION ($(date '+%m/%d/%Y'))"
+fi
